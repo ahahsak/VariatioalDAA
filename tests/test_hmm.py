a4149d2fd694cf48b557c3b690efd1021ce058d3
ahahsak <k.hayashi@em.ci.ritsumei.ac.jp>
2016-10-05 22:35:01 +0900

    edit tests

--- tests/test_hmm.py
+++ tests/test_hmm.py
@@ -4,61 +4,76 @@ from vbhmm.hmm import VB_HMM
 import vbhmm.util as util
 from nose.tools import assert_equal, ok_, nottest
 
+
 @nottest
 def test_allocate_fb():
     pass
+
 
 @nottest
 def test_forward():
     pass
 
+
 @nottest
 def test_backward():
     pass
+
 
 @nottest
 def test_initialize_vbhmm():
     pass
 
+
 @nottest
 def test__log_like_f():
     pass
+
 
 @nottest
 def test__calculate_sufficient_statistics():
     pass
 
+
 @nottest
 def test__update_parameters():
     pass
+
 
 @nottest
 def test__KL_div():
     pass
 
+
 @nottest
 def test_getExpectations():
     pass
+
 
 @nottest
 def test__Estep():
     pass
 
+
 @nottest
 def test_Mstep():
     pass
+
 
 @nottest
 def test__eval_hidden_states():
     pass
 
+
 @nottest
 def test_score():
     pass
 
+
 @nottest
 def test_fit():
     pass
+
 
 @nottest
 def test_showModel():
@@ -71,11 +86,12 @@ def test_showModel():
     z2 = model.decode(o2)
     model.show_model(True, True, True, True)
 
+
 @nottest
 def test_decode():
     pass
 
+
 @nottest
 def test_simulate():
     pass
-
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,48 +1,61 @@
-import numpy
+import numpy as np
 import vbhmm.util as util
 from nose.tools import assert_equal, ok_, nottest
+
 
 @nottest
 def test_logsum():
     pass
 
+
 @nottest
 def test_normalize():
     pass
+
 
 @nottest
 def test_KL_Dirichlet():
     pass
 
+
 def test_lnZ_Dirichlet():
-    upi = numpy.ones(10) * 0.5
-    wpi = numpy.array(upi)
+    upi = np.ones(10) * 0.5
+    wpi = np.array(upi)
     actual = util.lnZ_Dirichlet(wpi)
     ok_(2.54 < actual < 2.55)
 
+
 @nottest
 def test_lnZ_Wishart():
-    pass
+    nu = 0.1
+    V = np.atleast_2d(np.cov(10)) * 10
+    ok_(nu >= len(V) + 1)
+
 
 @nottest
 def test_log_like_Gauss():
     pass
 
+
 @nottest
 def test__sym_quad_form():
     pass
+
 
 @nottest
 def test_E_lndetW_Wishart():
     pass
 
+
 @nottest
 def test_KL_Wishart():
     pass
 
+
 @nottest
 def test_KL_GaussWishart():
     pass
+
 
 @nottest
 def test_sample_gaussian():
