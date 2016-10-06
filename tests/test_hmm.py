ef8a1affc49160ae24b90219f3164ac809df1edf
ahahsak <k.hayashi@em.ci.ritsumei.ac.jp>
2016-10-06 15:39:57 +0900

    remove lnA

--- tests/test_hmm.py
+++ tests/test_hmm.py
@@ -80,7 +80,7 @@ def test_showModel():
     model = VB_HMM(3)
     model.mu = np.array([[3.0, 3.0], [0.0, 0.0], [-4.0, 0.0]])
     model.cv = np.tile(np.identity(2), (3, 1, 1))
-    model.lnA = np.log([[0.9, 0.05, 0.05], [0.1, 0.7, 0.2], [0.1, 0.4, 0.5]])
+    # model._lnA = np.log([[0.9, 0.05, 0.05], [0.1, 0.7, 0.2], [0.1, 0.4, 0.5]])
     z, o2 = model.simulate(50 * 10)
     model.fit(o2)
     z2 = model.decode(o2)
