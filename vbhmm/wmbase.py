8ea173b8c681b0a65e8d213e1b8fc606254ba812
ahahsak <k.hayashi@em.ci.ritsumei.ac.jp>
2016-10-08 23:16:16 +0900

    rename directory

--- vbhmm/wmbase.py
+++
@@ -1,9 +0,0 @@
-# extend vbhmm to vb for word model
-from vbhmm.hmmbase import GaussianHmmBase
-import numpy as np
-
-
-class VbWordModel(GaussianHmmBase):
-
-    def __init__(self, N):
-        GaussianHmmBase.__init__(self, N)
