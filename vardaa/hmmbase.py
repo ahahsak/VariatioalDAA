43631e0faa6cc4c26351821c51ec43472b3f1081
ahahsak <k.hayashi@em.ci.ritsumei.ac.jp>
2016-10-08 23:12:19 +0900

    rename directory

---
+++ vardaa/hmmbase.py
@@ -0,0 +1,176 @@
+import numpy as np
+from numpy.random import rand, dirichlet, normal, random, randn
+from scipy.cluster import vq
+from scipy.special import gammaln, digamma
+from scipy.linalg import eig, inv, cholesky
+from vardaa.util import *
+
+"""
+    HMM with Gaussian emission probability.
+"""
+
+
+class GaussianHmmBase():
+
+    def __init__(self, N, UPI0=0.5, UA0=0.5, M0=0.0,
+                 BETA0=1, NU0=1, S0=0.01):
+
+        self.n_states = N
+        # log initial probability
+        self._lnpi = np.log(np.tile(1.0 / N, N))
+        # log transition probability
+        self._lnA = np.log(dirichlet([1.0] * N, N))
+
+    def _initialize_hmm(self, obs, scale=10.0):
+        n_states = self.n_states
+        self.A = dirichlet([1.0] * n_states, n_states)   # A:状態遷移行列
+        self.pi = np.tile(1.0 / n_states, n_states)  # pi:初期状態確率
+
+        T, D = obs.shape
+        self.mu, temp = vq.kmeans2(obs, n_states)
+        self.cv = np.tile(np.identity(D), (n_states, 1, 1))
+
+        """
+        if self._nu0 < D:
+            self._nu0 += D
+
+        self._m0 = np.mean(obs, 0)
+        self._v0 = np.atleast_2d(np.cov(obs.T)) * scale
+
+        # posterior for hidden states
+        self.z = dirichlet(np.tile(1.0 / n_states, n_states), T)
+        # for mean vector
+        self._m, temp = vq.kmeans2(obs, n_states, minit='points')
+        self._beta = np.tile(self._beta0, n_states)
+        # for covarience matrix
+        self._v = np.tile(np.array(self._v0), (n_states, 1, 1))
+        self._nu = np.tile(float(T) / n_states, n_states)
+
+        # aux valable
+        self._c = np.array(self._v)
+        """
+
+    def _allocate_fb(self, obs):
+        # fbアルゴリズムを走らせた時の一時保存用
+        T = len(obs)
+        lnalpha = np.zeros((T, self.n_states))  # log forward variable
+        lnbeta = np.zeros((T, self.n_states))  # log backward variable
+        lnxi = np.zeros((T - 1, self.n_states, self.n_states))
+        return lnalpha, lnbeta, lnxi
+
+    def _forward(self, lnf, lnalpha):
+        """
+        Use forward algorith to calculate forward variables and loglikelihood
+        input
+          lnF [ndarray, shape (n,n_states)] : loglikelihood of emissions
+          lnAlpha [ndarray, shape (n,n_states)] : log forward variable
+        output
+          lnAlpha [ndarray, shape (n,n_states)] : log forward variable
+          lnP [float] : lnP(X|theta)
+        """
+        T = len(lnf)
+        lnalpha *= 0.0
+        lnalpha[0, :] = self._lnpi + lnf[0, :]
+
+        for t in range(1, T):
+            lnalpha[t, :] = logsum(lnalpha[t - 1, :] +
+                                   self._lnA.T, 1) + lnf[t, :]
+
+        return lnalpha, logsum(lnalpha[-1, :])
+
+    def _backward(self, lnf, lnbeta):
+        """
+        Use backward algorith to calculate backward variables and loglikelihood
+        input
+            lnF [ndarray, shape (n,n_states)] : loglikelihood of emissions
+            lnBeta [ndarray, shape (n,n_states)] : log backward variable
+        output
+            lnBeta [ndarray, shape (n,n_states)] : log backward variable
+            lnP [float] : lnP(X|theta)
+        """
+        T = len(lnf)
+        lnbeta[T - 1, :] = 0.0
+
+        for t in range(T - 2, -1, -1):
+            lnbeta[t, :] = logsum(
+                self._lnA + lnf[t + 1, :] + lnbeta[t + 1, :], 1)
+
+        return lnbeta, logsum(lnbeta[0, :] + lnf[0, :] + self._lnpi)
+
+    def _log_like_f(self, obs):
+        pass
+
+    def _eval_hidden_states(self, obs):
+        """
+        Estep
+        Then obtain variational free energy and posterior over hidden states
+        """
+
+        lnf = self._log_like_f(obs)
+        lnalpha, lnbeta, lnxi = self._allocate_fb(obs)
+        lnxi, lngamma, lnp = self._e_step(lnf, lnalpha, lnbeta, lnxi)
+        z = np.exp(lngamma)
+        return z, lnp
+
+    def _e_step(self, lnf, lnalpha, lnbeta, lnxi):
+        """
+        lnF [ndarray, shape (n,n_states)] : loglikelihood of emissions
+        lnAlpha [ndarray, shape (n, n_states]: log forward message
+        lnBeta [ndarray, shape (n, n_states)]: log backward message
+        lnPx_f: log sum of p(x_n) by forward message for scalling
+        lnPx_b: log sum of p(x_n) by backward message for scalling
+        """
+        T = len(lnf)
+        # forward-backward algorithm
+        lnalpha, lnpx_f = self._forward(lnf, lnalpha)
+        lnbeta, lnpx_b = self._backward(lnf, lnbeta)
+
+        # check if forward and backward were done correctly
+        dlnp = lnpx_f - lnpx_b
+        if abs(dlnp) > 1.0e-6:
+            print("warning forward and backward are not equivalent")
+
+        # compute lnXi for updating transition matrix
+        for i in range(self.n_states):
+            for j in range(self.n_states):
+                for t in range(T - 1):
+                    lnxi[t, i, j] = lnalpha[t, i] + self._lnA[i, j, ] + \
+                        lnf[t + 1, j] + lnbeta[t + 1, j]
+        lnxi -= lnpx_f
+
+        # compute lnGamma for postetior on hidden states
+        lngamma = lnalpha + lnbeta - lnpx_f
+
+        return lnxi, lngamma, lnpx_f
+
+    def _m_step(self, obs, lnxi, lngamma):
+        self._calculate_sufficient_statistics(obs, lnxi, lngamma)
+        self._update_parameters(obs, lnxi, lngamma)
+
+    def _calculate_sufficient_statistics(self, obs, lnxi, lngamma):
+        pass
+
+    def _update_parameters(self, obs, lnxi, lngamma):
+        pass
+
+    def decode(self, obs):
+        """
+        Get the most probable cluster id
+        """
+        z, lnp = self._eval_hidden_states(obs)
+        return z.argmax(1)
+
+    def simulate(self, T, mu, cv):
+        n, d = mu.shape
+
+        pi_cdf = np.exp(self._lnpi).cumsum()
+        A_cdf = np.exp(self._lnA).cumsum(1)
+        z = np.zeros(T, dtype=np.int)
+        o = np.zeros((T, d))
+        r = random(T)
+        z[0] = (pi_cdf > r[0]).argmax()
+        o[0] = sample_gaussian(mu[z[0]], cv[z[0]])
+        for t in range(1, T):
+            z[t] = (A_cdf[z[t - 1]] > r[t]).argmax()
+            o[t] = sample_gaussian(mu[z[t]], cv[z[t]])
+        return z, o
---
+++ vardaa/util.py
@@ -0,0 +1,203 @@
+import numpy as np
+from scipy.special import gammaln, digamma
+from scipy.linalg import eig, det, solve, inv, cholesky
+from scipy.spatial.distance import cdist
+from numpy.random import randn
+
+
+def logsum(A, axis=None):
+    """
+    Computes the sum of A assuming A is in the log domain.
+    Returns log(sum(exp(A), axis)) while minimizing the possibility of
+    over/underflow.
+    """
+    Amax = A.max(axis)
+    if axis and A.ndim > 1:
+        shape = list(A.shape)
+        shape[axis] = 1
+        Amax.shape = shape
+    Asum = np.log(np.sum(np.exp(A - Amax), axis))
+    Asum += Amax.reshape(Asum.shape)
+    if axis:
+        # Look out for underflow.
+        Asum[np.isnan(Asum)] = - np.Inf
+    return Asum
+
+
+def normalize(A, axis=None):
+    A += np.finfo(float).eps
+    Asum = A.sum(axis)
+    if axis and A.ndim > 1:
+        # Make sure we don't divide by zero.
+        Asum[Asum == 0] = 1
+        shape = list(A.shape)
+        shape[axis] = 1
+        Asum.shape = shape
+    return A / Asum
+
+
+def kl_dirichlet(alpha1, alpha2):
+    """
+    KL-div of Dirichlet distribution KL[q(alpha1)||p(alpha2)]
+    input
+      alpha1 [ndarray, shape (nmix)] : parameter of 1st Dirichlet dist
+      alpha2 [ndarray, shape (nmix)] : parameter of 2nd Dirichlet dist
+    """
+
+    kl = - lnz_dirichlet(alpha1) + lnz_dirichlet(alpha2) \
+        + np.dot((alpha1 - alpha2), (digamma(alpha1) - digamma(alpha1.sum())))
+
+    return kl
+
+
+def lnz_dirichlet(alpha):
+    """
+    log normalization constant of Dirichlet distribution
+    input
+      alpha [ndarray, shape (nmix)] : parameter of Dirichlet distribution
+    """
+
+    Z = gammaln(alpha).sum() - gammaln(alpha.sum())
+    return Z
+
+
+def lnz_wishart(nu, V):
+    """
+    log normalization constant of Wishart distribution
+    input
+      nu [float] : dof parameter of Wichart distribution
+      V [ndarray, shape (D x D)] : base matrix of Wishart distribution
+      note <CovMat> = V/nu
+    """
+    '''
+    if nu < len(V) + 1:
+        raise ValueError("dof parameter nu must larger than len(V)")
+    '''
+    D = len(V)
+    lnZ = 0.5 * nu * (D * np.log(2.0) - np.log(det(V))) \
+        + gammaln(np.arange(nu + 1 - D, nu + 1) * 0.5).sum()
+
+    return lnZ
+
+
+def log_like_gauss(obs, nu, V, beta, m):
+    """
+    Log probability for Gaussian with full covariance matrices.
+    Here mean vectors and covarience matrices are probability variable with
+    respect to Gauss-Wishart distribution.
+    """
+    nobs, ndim = obs.shape
+    nmix = len(m)
+    lnf = np.empty((nobs, nmix))
+    for k in range(nmix):
+        dln2pi = ndim * np.log(2.0 * np.pi)
+        lndetV = - e_lndetw_wishart(nu[k], V[k])
+        cv = V[k] / nu[k]
+        q = _sym_quad_form(obs, m[k], cv) + ndim / beta[k]
+        lnf[:, k] = -0.5 * (dln2pi + lndetV + q)
+
+    return lnf
+
+
+def _sym_quad_form(x, mu, A):
+    """
+    calculate x.T * inv(A) * x
+    """
+    q = (cdist(x, mu[np.newaxis], "mahalanobis", VI=inv(A))**2).reshape(-1)
+    return q
+
+
+def e_lndetw_wishart(nu, V):
+    """
+    mean of log determinant of precision matrix over Wishart <lndet(W)>
+    input
+      nu [float] : dof parameter of Wichart distribution
+      V [ndarray, shape (D x D)] : base matrix of Wishart distribution
+    """
+
+    D = len(V)
+    E = D * np.log(2.0) - np.log(det(V)) + \
+        digamma(np.arange(nu + 1 - D, nu + 1) * 0.5).sum()
+
+    return E
+
+
+def kl_wishart(nu1, V1, nu2, V2):
+    """
+    KL-div of Wishart distribution KL[q(nu1,V1)||p(nu2,V2)]
+    """
+    '''
+    if nu1 < len(V1) + 1:
+        raise ValueError("dof parameter nu1 must larger than len(V1)")
+    if nu2 < len(V2) + 1:
+        raise ValueError("dof parameter nu2 must larger than len(V2)")
+
+    if len(V1) != len(V2):
+        raise ValueError("dimension of two matrix dont match, %d and %d" % (
+            len(V1), len(V2)))
+    '''
+
+    D = len(V1)
+    kl = 0.5 * ((nu1 - nu2) * e_lndetw_wishart(nu1, V1) + nu1 *
+                (np.trace(solve(V1, V2)) - D)) - lnz_wishart(nu1, V1)
+    + lnz_wishart(nu2, V2)
+
+    '''
+    if KL < _small_negative_number:
+        print(nu1, nu2, V1, V2)
+        raise ValueError("KL must be larger than 0")
+    '''
+    return kl
+
+
+def kl_gauss_wishart(nu1, V1, beta1, m1, nu2, V2, beta2, m2):
+    """
+    KL-div of Gauss-Wishart distr KL[q(nu1,V1,beta1,m1)||p(nu2,V2,beta2,m2)
+    """
+    if len(m1) != len(m2):
+        raise ValueError(
+            "dimension of two mean dont match, %d and %d" % (len(m1), len(m2)))
+
+    D = len(m1)
+
+    # first assign KL of Wishart
+    kl1 = kl_wishart(nu1, V1, nu2, V2)
+
+    # the rest terms
+    kl2 = 0.5 * (D * (np.log(beta1 / float(beta2)) + beta2 / float(beta1) -
+                      1.0) + beta2 * nu1 * np.dot((m1 - m2),
+                                                  solve(V1, (m1 - m2))))
+
+    kl = kl1 + kl2
+    '''
+    if KL < _small_negative_number:
+        raise ValueError("KL must be larger than 0")
+    '''
+    return kl
+
+
+def sample_gaussian(m, cv, n=1):
+    """Generate random samples from a Gaussian distribution.
+    Parameters
+    ----------
+    m : array, shape (ndim)
+        Mean of the distribution.
+    cv : array, shape (ndim,ndim)
+        Covariance of the distribution.
+    n : int, optional
+        Number of samples to generate. Defaults to 1.
+    Returns
+    -------
+    obs : array, shape (ndim, n)
+        Randomly generated sample
+    """
+
+    ndim = len(m)
+    r = randn(n, ndim)
+    if n == 1:
+        r.shape = (ndim,)
+
+    cv_chol = cholesky(cv)
+    r = np.dot(r, cv_chol.T) + m
+
+    return r
