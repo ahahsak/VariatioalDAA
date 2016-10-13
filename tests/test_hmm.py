89fc143ec1a39ce8229b05fe89b5510fe6e862e9
ahahsak <k.hayashi@em.ci.ritsumei.ac.jp>
2016-10-13 16:19:52 +0900

    add tests

--- tests/test_hmm.py
+++ tests/test_hmm.py
@@ -46,11 +46,6 @@ def test__KL_div():
 
 
 @nottest
-def test_getExpectations():
-    pass
-
-
-@nottest
 def test__Estep():
     pass
 
@@ -66,29 +61,7 @@ def test__eval_hidden_states():
 
 
 @nottest
-def test_score():
-    pass
-
-
-@nottest
 def test_fit():
-    pass
-
-
-@nottest
-def test_showModel():
-    model = VbHmm(3)
-    mu = np.array([[3.0, 3.0], [0.0, 0.0], [-4.0, 0.0]])
-    cv = np.tile(np.identity(2), (3, 1, 1))
-    # model.lnA = np.log([[0.9, 0.05, 0.05], [0.1, 0.7, 0.2], [0.1, 0.4, 0.5]])
-    z, o2 = model.simulate(50 * 10, mu, cv)
-    model.fit(o2)
-    z2 = model.decode(o2)
-    model.show_model(True, True, True, True)
-
-
-@nottest
-def test_decode():
     pass
 
 
