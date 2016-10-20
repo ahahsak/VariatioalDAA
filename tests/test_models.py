21dd4d2acb24481d8fa69cfdc452a161d0e720eb
ahahsak <k.hayashi@em.ci.ritsumei.ac.jp>
2016-10-20 14:13:35 +0900

    add test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,31 +7,26 @@ import vardaa.util as util
 from nose.tools import assert_equal, ok_, nottest
 
 
-@nottest
 def test_show():
     model = VbHmm(3)
     mu = np.array([[3.0, 3.0], [0.0, 0.0], [-4.0, 0.0]])
     cv = np.tile(np.identity(2), (3, 1, 1))
-    # model.lnA = np.log([[0.9, 0.05, 0.05], [0.1, 0.7, 0.2], [0.1, 0.4, 0.5]])
+    model.lnA = np.log(
+        [[0.9, 0.01, 0.09], [0.09, 0.9, 0.01], [0.09, 0.01, 0.9]])
     z, o2 = model.simulate(50 * 10, mu, cv)
     model.fit(o2)
-    result = Model(model.pi, model.A, model.mu, model.cv,
-                   model._wa, model._nu, model._v, model._m)
-    # z2 = model.decode(o2)
-    # model.show_model(True, True, True, True)
+    result = Model(model._wa, model._nu, model._v, model._m)
     result.show()
 
 
-@nottest
 def test_decode():
     model = VbHmm(3)
     mu = np.array([[3.0, 3.0], [0.0, 0.0], [-4.0, 0.0]])
     cv = np.tile(np.identity(2), (3, 1, 1))
-    # model.lnA = np.log([[0.9, 0.05, 0.05], [0.1, 0.7, 0.2], [0.1, 0.4, 0.5]])
+    model.lnA = np.log([[0.9, 0.05, 0.05], [0.1, 0.7, 0.2], [0.1, 0.4, 0.5]])
     z, o2 = model.simulate(50 * 10, mu, cv)
     model.fit(o2)
-    result = Model(model.pi, model.A, model.mu, model.cv,
-                   model._wa, model._nu, model._v, model._m)
+    result = Model(model._wa, model._nu, model._v, model._m)
     codes = result.decode(model.z)
     pass
 
